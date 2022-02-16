1712256fe4984b88479e3f6a6b4da544ab2ae157
Matilde Dematei <m.dematei@hotmail.com>
2022-02-16 15:15:49 -0300

    Pantalla Login

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,58 @@
+import React, {useState} from "react";
+import './Login.css';
+import Input from "./Components/Input/Input";
+
+
+const Login = () => {
+
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleChange(name, value) {
+        if(name === 'Usuario') {
+            setUser(value)
+        } 
+        else {
+            setPassword(value)
+        }
+    }
+
+    function handleSubmit() {
+        let account = {user, password}
+        if(account) {
+            console.log('Account:', account)
+        }
+    }
+
+    return (
+            <div className="login-container">
+                <div>
+                    <img src={'images/logo-pluspagos.png'} />
+                </div>
+                
+                <Input 
+                attribute={{
+                    id: 'Usuario',
+                    name: 'Usuario',
+                    type: 'text',
+                    placeholder: 'Usuario'
+                }}
+                handleChange={handleChange}
+                />
+                <Input 
+                attribute={{
+                    id: 'Contraseña',
+                    name: 'Contraseña',
+                    type: 'password',
+                    placeholder: 'Contraseña'
+                }}
+                handleChange={handleChange}
+                />
+                <button onClick={handleSubmit} className="login-btn">
+                    Ingresar
+                </button>
+            </div>
+    )
+}
+
+export default Login
